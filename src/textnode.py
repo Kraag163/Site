e85a89d71d6fce04c26fba11c56aa87096f21e3a
Kraag163 <mulder163@yahoo.fr>
2025-11-21 20:17:10 +0100

    CH2 L4

--- src/textnode.py
+++ src/textnode.py
@@ -9,6 +9,12 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+# text (plain)
+# **Bold text** <b>                             <b>bold</b>
+# _Italic text_ <i>                             <i>italic</i>
+# `Code text`                                   <code>This is code</code>
+# Links, in this format: [anchor text](url)     <a href="https://www.google.com">link</a>.
+# Images, in this format: ![alt text](url)      <img src="url/of/image.jpg" alt="Description of image" />
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
